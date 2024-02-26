a5f4567b10fce81a2ff1587b9b509188db48a908
Kulkarni Sai Pooja <kulkarni.saipooja@publicissapient.com>
2024-02-27 05:24:33 +0530

    Added

---
+++ src/main/java/com/jenkins/JenkinsDemoApplication.java
@@ -0,0 +1,14 @@
+package com.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+		System.out.println("HEY FROM JENKINS");
+	}
+
+}
